3c20afc9a25988aafe91359a4718cf0924ae5849
JuanCruzMedina <juancruzfaq@gmail.com>
2025-04-07 18:02:50 -0300

    create project structure

---
+++ src/app/(shop)/empty/page.tsx
@@ -0,0 +1,3 @@
+export default function EmptyCartPage() {
+  return <div>Empty cart</div>;
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className={`${titleFont.className} text-4xl font-bold`}>Register</h1>
+    </div>
+  );
+}
