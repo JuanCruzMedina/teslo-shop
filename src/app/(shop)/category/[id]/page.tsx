409cb96ab41125e6211e2484f2eb054d59e439f4
JuanCruzMedina <juancruzfaq@gmail.com>
2025-04-08 11:55:25 -0300

    feat: update category page to include product grid and titles for categories

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,12 +1,20 @@
-import { notFound } from "next/navigation";
+import { ProductGrid } from "@/components/products/product-grid/ProductGrid";
+import { Title } from "@/components/ui/title/Title";
+import { Category, Product } from "@/interfaces/product.interface";
+import { initialData } from "@/seed/seed";
 
 interface CategoryPageProps {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
-const validCategories = ["men", "women", "kids"];
+const getProductsByCategory = async (category: Category) => {
+  const filteredProducts = initialData.products.filter(
+    (product) => product.gender === category
+  );
+  return filteredProducts;
+};
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { id } = await params;
@@ -14,10 +22,31 @@ export default async function CategoryPage({ params }: CategoryPageProps) {
   if (!id) {
     throw new Error("Category ID is required");
   }
-  if (!validCategories.includes(id)) {
-    notFound();
-  }
+  // if (ValidCategories.indexOf(id as ValidCategories) === -1) {
+  //   notFound();
+  // }
 
+  const products = await getProductsByCategory(id);
+  const titleByCategory: Record<Category, string> = {
+    men: "Men's Collection",
+    women: "Women's Collection",
+    kid: "Kids' Collection",
+    unisex: "Unisex Collection",
+  };
+  const subtitleByCategory: Record<Category, string> = {
+    men: "Explore our men's products",
+    women: "Discover our women's range",
+    kid: "Check out our kids' items",
+    unisex: "Shop our unisex selection",
+  };
 
-  return <div>Category: {id}</div>;
+  return (
+    <div>
+      <Title
+        title={titleByCategory[id]}
+        subtitle={subtitleByCategory[id]}
+      ></Title>
+      <ProductGrid products={products as Product[]} />
+    </div>
+  );
 }
