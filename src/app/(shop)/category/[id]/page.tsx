f977502df8fa2ebc4e16d7ae71bbcc26a7e2f070
JuanCruzMedina <juancruzfaq@gmail.com>
2025-04-07 22:34:14 -0300

    refactor: create categroies not found page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,3 +1,23 @@
-export default function CategoryPage() {
-  return <div>Category</div>;
+import { notFound } from "next/navigation";
+
+interface CategoryPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const validCategories = ["men", "women", "kids"];
+
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const { id } = await params;
+
+  if (!id) {
+    throw new Error("Category ID is required");
+  }
+  if (!validCategories.includes(id)) {
+    notFound();
+  }
+
+
+  return <div>Category: {id}</div>;
 }
