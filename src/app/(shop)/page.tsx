09cd261fe88d9852a46c096505b83ead255f693f
JuanCruzMedina <juancruzfaq@gmail.com>
2025-04-08 10:16:29 -0300

    feat: create title component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,9 @@
-import { titleFont } from "@/config/fonts";
+import { Title } from "@/components/ui/title/Title";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <p className={`${titleFont.className} text-4xl font-bold`}>
-        This is a title
-      </p>
+    <div>
+      <Title title={"Shop"} subtitle={"Welcome to the shop!"}></Title>
     </div>
   );
 }
