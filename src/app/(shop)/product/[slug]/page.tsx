602557dac6f9123a6fa2cb4df74b0bf80cbade0a
JuanCruzMedina <juancruzfaq@gmail.com>
2025-04-08 12:10:08 -0300

    feat: implement product page with image display and add to cart button

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,3 +1,47 @@
-export default function ProductPage() {
-  return <div>Product</div>;
+import { buttonStyles } from "@/app/styles";
+import { titleFont } from "@/config/fonts";
+import { Product } from "@/interfaces/product.interface";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+interface ProductPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+function getProductBySlug(slug: string) {
+  const product = initialData.products.find((product) => product.slug === slug);
+  return product as Product;
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { slug } = await params;
+  console.log("slug", slug);
+  const product: Product = getProductBySlug(slug);
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      <div className="col-span-1 md:col-span-2">
+        <Image
+          src={`/products/${product.images[0]}`}
+          alt={product.title}
+          width={800}
+          height={800}
+        ></Image>
+      </div>
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+        <button className={`${buttonStyles.primary} my-6`}>Add to Cart</button>
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/styles.ts
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+
+
+export const buttonStyles = {
+    primary: clsx(
+        "bg-blue-600",
+        "hover:bg-blue-800",
+        "text-white",
+        "py-2",
+        "px-4",
+        "rounded",
+        "transition-all"
+    ),
+    secondary: clsx(
+        "bg-gray-200",
+        "hover:bg-gray-300",
+        "text-black",
+        "py-2",
+        "px-4",
+        "rounded",
+        "transition-all"
+    ),
+};
