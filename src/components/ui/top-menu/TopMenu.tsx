61bd518879caf57e04f193166b6829c2ef59850b
JuanCruzMedina <juancruzfaq@gmail.com>
2025-05-14 00:40:40 -0300

    feat: update cart link behavior and add fade-in effect for item count

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -53,10 +53,13 @@ export const TopMenu = () => {
         <Link href="/search" className="mx-2">
           <IoSearchOutline className="w-5 h-5" />
         </Link>
-        <Link href="/cart" className="mx-2">
+        <Link
+          href={loaded && totalItems > 0 ? "/cart" : "/empty"}
+          className="mx-2"
+        >
           <div className="relative">
             {loaded && totalItems > 0 && (
-              <span className="absolute text-xs px-1 rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white">
+              <span className="absolute text-xs px-1 rounded-full font-bold -top-2 -right-2 bg-blue-700 text-white fade-in">
                 {totalItems}
               </span>
             )}
