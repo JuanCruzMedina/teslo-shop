af7cb318986eda88c0e891ae95eb28949a6fbd6d
JuanCruzMedina <juancruzfaq@gmail.com>
2025-05-16 12:56:00 -0300

    chore: add orders deleting from database seed

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -7,6 +7,9 @@ async function seedDatabase() {
     console.log("Seeding database...");
     // Delete existing data
     await Promise.all([
+        await prisma.orderAddress.deleteMany(),
+        await prisma.orderItem.deleteMany(),
+        await prisma.order.deleteMany(),
         await prisma.userAddress.deleteMany(),
         await prisma.country.deleteMany(),
         await prisma.user.deleteMany(),
